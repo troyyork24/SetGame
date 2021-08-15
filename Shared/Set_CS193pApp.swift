40b1c73c26c2028f29adea812c38a13313b07a08
Troy York <66917393+troyyork24@users.noreply.github.com>
2021-08-15 02:15:38 -0500

    Initial Commit

---
+++ Shared/Set_CS193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_CS193pApp.swift
+//  Shared
+//
+//  Created by Troy Thomas York on 8/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Set_CS193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
